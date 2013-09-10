9f8cc98924aa549d8705b40c54fc54a2b5865814
Ghislain Vaillant <ghisvail@gmail.com>
2013-09-10 11:01:46 +0100

    initial import of the project

---
+++ itsense/itsense.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+module pysense.solver
+"""
+from __future__ import division
+import numpy as np
+from pykrylov.linop import (BlockLinearOperator, BlockDiagonalLinearOperator,
+                            DiagonalOperator, IdentityOperator, LinearOperator)
+from pykrylov.cg import CG
+from pynfft import NFFT
+
+
+__all__ = ['IterativeSenseSolver',]
+
+
+class NfftOperator(object):
+    """
+    Wrapper class for the pynfft.NFFT object used by the iterative SENSE \
+    solver.
+    """
+    def __init__(self, plan):
+        self.plan = plan
+        self.nargin = self.plan.N_total
+        self.nargout = self.plan.M_total
+    def direct(self, f_hat):
+        self.plan.f_hat = f_hat
+        self.plan.trafo()
+        return self.plan.f
+    def adjoint(self, f):
+        self.plan.f = f
+        self.plan.adjoint()
+        return self.plan.f_hat
+
+
+class IterativeSenseSolver(object):
+    def __init__(self, k, s, d=None, niter=10, nc=None, nk=None, N=None,
+                 **kwargs):
+        """
+        TODO: empty docsting
+        """
+        self.nk = nk or np.prod(k.shape[:-1])
+        self.nc = nc or s.shape[0]
+        self.N = N or s.shape[1:]
+        self.k = k
+        self.s = s
+        self.d = d
+        self.niter = niter
+
+        # precompute shared plan for each FT operations
+        self._plan = NFFT(N=self.N, M=self.nk)
+        self._plan.x = k
+
+        # define linear operators for iterative SENSE algo
+        nk = self.nk
+        nc = self.nc        
+        d = self.d.ravel() if self.d is not None else None
+        s = self.s.reshape([nc, -1])
+        
+        if d is not None:
+            D_diag = DiagonalOperator(diag=d)
+        else:
+            D_diag = IdentityOperator(nargin=nk)            
+        blocks = [D_diag] * nc
+        self._D = BlockDiagonalLinearOperator(blocks)
+
+        diag = 1.0 / np.sqrt((s ** 2).sum(axis=0))
+        diag = diag.ravel()        
+        self._I = DiagonalOperator(diag=diag)
+
+        Nfft = NfftOperator(self._plan)
+        F = LinearOperator(nargin=Nfft.nargin, nargout=Nfft.nargout,
+                           matvec=Nfft.direct, matvec_transp=Nfft.adjoint)
+        blocks = [[F * DiagonalOperator(_s)] for _s in s]
+        self._E = BlockLinearOperator(blocks)
+
+        # solver is initialized separately, avoid long object creation time
+        self.solver = None
+
+    def __call__(self, m):
+        """
+        TODO: empty docstring
+        """
+        self.solve(m)
+        return self.solution
+
+    def initialize(self):
+        """
+        TODO: empty docsting
+        """
+        # precompute NFFT plan
+        self._plan.precompute()
+        # initialize CG solver
+        D, E, I = (self._D, self._E, self._I)
+        A = I * E.T * D * E * I
+        matvec = lambda x: A * x
+        self.solver = CG(matvec=matvec, matvec_max=self.niter)        
+
+    def solve(self, m):
+        """
+        TODO: empty docsting
+        """
+        if self.solver is None:
+            raise RuntimeError("Attempted to use non-initialized solver")
+        D, E, I = (self._D, self._E, self._I)        
+        b = I * E.T * D * m
+        self.rhs = b
+        self.solver.solve(b)
+        b_approx = self.solver.bestSolution
+        v_approx = I * b_approx
+        self.solution = v_approx
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup
+
+if sys.version_info[0] < 3:
+    import __builtin__ as builtins
+else:
+    import builtins
+
+builtins.__PACKAGE_SETUP__ = True
+
+import itsense 
+VERSION = itsense.__version__
+
+DISTNAME = 'iterative-sense'
+DESCRIPTION = 'A Python implementation of the iterative SENSE solver'
+LONG_DESCRIPTION = open('README.rst').read()
+MAINTAINER = 'Ghislain Vaillant'
+MAINTAINER_EMAIL = 'ghisvail@gmail.com'
+LICENSE = 'GPL'
+
+def setup_package():
+    metadata = dict(
+        name=DISTNAME,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        install_requires=['pykrylov', 'pynfft'],
+        license=LICENSE,
+        long_description=LONG_DESCRIPTION,
+        packages=['itsense',],
+        version=VERSION,)
+    setup(**metadata)
+
+if __name__ == "__main__":
+    setup_package()
