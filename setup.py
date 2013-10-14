81edf658dccab93f10412b042d12c72bcbeff1b0
Ghislain Vaillant <ghisvail@gmail.com>
2013-10-14 14:46:00 +0100

    fix version string handling

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
-import sys
 from setuptools import setup
 
-if sys.version_info[0] < 3:
-    import __builtin__ as builtins
-else:
-    import builtins
+package_name = 'itsense'
 
-builtins.__PACKAGE_SETUP__ = True
+import imp
+ver_file, ver_pathname, ver_description = imp.find_module(
+            '_version', [package_name])
+try:
+    _version = imp.load_module('version', ver_file, ver_pathname,
+            ver_description)
+finally:
+    ver_file.close()
 
-import itsense 
-VERSION = itsense.__version__
+version = _version.version
 
 DISTNAME = 'iterative-sense'
 DESCRIPTION = 'A Python implementation of the iterative SENSE solver'
@@ -18,6 +20,19 @@ LONG_DESCRIPTION = open('README.md').read()
 MAINTAINER = 'Ghislain Vaillant'
 MAINTAINER_EMAIL = 'ghisvail@gmail.com'
 LICENSE = 'GPL'
+URL = 'https://github.com/ghisvail/iterative-sense'
+
+classifiers = [
+    'Programming Language :: Python',
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Operating System :: POSIX :: Linux',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Multimedia :: Sound/Audio :: Analysis',
+]
 
 def setup_package():
     metadata = dict(
@@ -25,11 +40,15 @@ def setup_package():
         maintainer=MAINTAINER,
         maintainer_email=MAINTAINER_EMAIL,
         description=DESCRIPTION,
-        install_requires=['pykrylov', 'pynfft'],
+        install_requires=[
+            "pykrylov >= 0.1.1",
+            "pynfft >= 1.1",],
         license=LICENSE,
         long_description=LONG_DESCRIPTION,
-        packages=['itsense',],
-        version=VERSION,)
+        url=URL,
+        classifiers=classifiers,
+        packages=[package_name],
+        version=version,)
     setup(**metadata)
 
 if __name__ == "__main__":
