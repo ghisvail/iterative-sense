590da81784423a1843b0cad46e1f901c2cd84c5e
Ghislain Vaillant <ghisvail@gmail.com>
2013-10-14 14:45:14 +0100

    fix version string handling

--- itsense/__init__.py
+++ itsense/__init__.py
@@ -1,16 +1,6 @@
 """
 itsense: a Python implementation of the iterative SENSE solver
 """
+from ._version import version as __version__
+from .itsense import IterativeSenseSolver
 
-__version__ = '0.1.0'
-
-try:
-    # This variable is injected in the __builtins__ by the build
-    # process.
-    __PACKAGE_SETUP__
-except NameError:
-    __PACKAGE_SETUP__ = False
-
-if not __PACKAGE_SETUP__:
-    from .itsense import IterativeSenseSolver
-    __all__ = ['IterativeSenseSolver',]
---
+++ itsense/_version.py
@@ -0,0 +1,9 @@
+"""
+Standard version management
+"""
+
+version = '0.1.0'
+release = False
+
+if not release:
+    version += '-dev'
